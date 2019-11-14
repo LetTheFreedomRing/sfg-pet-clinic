1811ac7d49791c0d6b55db7a3cd2bfc081e317d9
Pavlo Kolesnichenko <pavlo.kolesnichenko@gmail.com>
2019-11-14 11:35:23 +0200

    added vet speciality entity. Closes #27

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}

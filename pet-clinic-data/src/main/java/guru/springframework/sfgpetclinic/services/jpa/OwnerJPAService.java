174640e5bb513cc9e3b39ed2fe9f589da601e5a4
Pavlo Kolesnichenko <pavlo.kolesnichenko@gmail.com>
2019-11-29 17:55:50 +0200

    added Owner JPA Service. Closes #40

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/jpa/OwnerJPAService.java
@@ -0,0 +1,62 @@
+package guru.springframework.sfgpetclinic.services.jpa;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.repositories.OwnerRepository;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class OwnerJPAService implements OwnerService {
+
+    private final OwnerRepository ownerRepository;
+
+    public OwnerJPAService(OwnerRepository ownerRepository) {
+        this.ownerRepository = ownerRepository;
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        Optional<Owner> owner = ownerRepository.findByLastName(lastName);
+        if (!owner.isPresent()) {
+            throw new RuntimeException("Entity not found");
+        }
+        return owner.get();
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+        ownerRepository.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        Optional<Owner> owner = ownerRepository.findById(id);
+        if (!owner.isPresent()) {
+            throw new RuntimeException("Entity not found");
+        }
+        return owner.get();
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return ownerRepository.save(owner);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        ownerRepository.delete(owner);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        ownerRepository.deleteById(id);
+    }
+}
